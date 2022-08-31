8f187d232213538d6bbdea8b5cea878a16d7e5fb
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-08-31 22:05:00 +0530

    Create java operator.cpp

---
+++ java operator.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    void relationalOperators(int A,int B)
+    {
+        if(A>B){
+        cout<<A<<" is greater than "<<B<<endl;
+        }
+         else if(A<B){
+        cout<<A<<" is less than "<<B<<endl;
+         }
+        else{
+        cout<<A<<" equal to "<<B<<endl;
+    }
+    }
+};
